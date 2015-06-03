b99f85a01744205acecebcec9254c411cf56d76d
Ben Krieger <blitzrk@gmail.com>
2015-06-03 10:33:38 -0600

    added score sort test

--- ga/sort_test.go
+++ ga/sort_test.go
@@ -5,39 +5,61 @@ import (
 	"testing"
 )
 
-func TestMergeSortedDesc(t *testing.T) {
-	tests := []struct {
-		in  pairs
-		out pairs
-	}{
-		{
-			pairs{
-				pair{1, 1.5},
-				pair{2, 2.0},
-			},
-			pairs{
-				pair{2, 2.0},
-				pair{1, 1.5},
-			},
+var tests = []struct {
+	in  pairs
+	out pairs
+}{
+	{
+		pairs{
+			pair{1, 1.5},
+			pair{2, 2.0},
 		},
-		{
-			pairs{
-				pair{1, 6.6},
-				pair{2, 12.0},
-				pair{3, 7.0},
-				pair{4, 5.5},
-				pair{5, 8.0},
-			},
-			pairs{
-				pair{2, 12.0},
-				pair{5, 8.0},
-				pair{3, 7.0},
-				pair{1, 6.6},
-				pair{4, 5.5},
-			},
+		pairs{
+			pair{2, 2.0},
+			pair{1, 1.5},
 		},
+	},
+	{
+		pairs{
+			pair{1, 6.6},
+			pair{2, 12.0},
+			pair{3, 7.0},
+			pair{4, 5.5},
+			pair{5, 8.0},
+		},
+		pairs{
+			pair{2, 12.0},
+			pair{5, 8.0},
+			pair{3, 7.0},
+			pair{1, 6.6},
+			pair{4, 5.5},
+		},
+	},
+}
+
+func (ps1 pairs) equals(ps2 pairs) bool {
+	if len(ps1) != len(ps2) {
+		return false
 	}
 
+	for i := 0; i < len(ps1); i++ {
+		if ps1[i].item != ps2[i].item {
+			return false
+		}
+	}
+	return true
+}
+
+func TestSort(t *testing.T) {
+	for i := 0; i < len(tests); i++ {
+		sort.Sort(sort.Reverse(byScore(tests[i].in)))
+		if !tests[i].in.equals(tests[i].out) {
+			t.Errorf("Sorting failed, got %v, expected %v", tests[i].in, tests[i].out)
+		}
+	}
+}
+
+func TestMergeSortedDesc(t *testing.T) {
 	for i := 1; i < len(tests); i++ {
 		expect := make(pairs, len(tests[i].out))
 		copy(expect, tests[i].out)
