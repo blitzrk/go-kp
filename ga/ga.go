389238869e73f59bcd044d8a0314c1eb6ed2ed7a
Ben Krieger <blitzrk@gmail.com>
2015-06-04 09:39:58 -0600

    ga documentation and RunResult struct

--- ga/ga.go
+++ ga/ga.go
@@ -1,13 +1,25 @@
+// Implements a general genetic algorithm for use in solving 0-1 knapsack
+// problems.
 package ga
 
 import (
 	"bytes"
 	"fmt"
 	"math/rand"
+	"sort"
 )
 
+// A chromosome is represented as a byte slice where each byte stores a
+// character from the genome's alphabet. So for a binary problem, it may store
+// 0x0s and 0x1s, and for a genetics problem it may store A, T, C, and G (as
+// 0x0, 0x1, 0x2, 0x3).
 type Chromosome []byte
 
+// This interface is for a construct that can find the fitness value of a given
+// chromosome. The reason for having a struct to perform the calculations
+// instead of a closure (because both can manage caching) is to allow wrapping
+// in related functions that use Fitness such as greedy algorithms for
+// optimizing initial populations.
 type Performance interface {
 	Fitness(Chromosome) float64
 }
@@ -26,6 +38,9 @@ func (c Chromosome) String() string {
 	return b.String()
 }
 
+// All available parameters for tweaking the way that the algorithm works are
+// manipulated through Parameters. Uint varieties are used to help inform what
+// the expected size of each parameter should be.
 type Parameters struct {
 	Performance
 	ChromLen   uint
@@ -48,7 +63,26 @@ func randChromosomeFunc(chars, clen int) func() Chromosome {
 	}
 }
 
-func Run(p *Parameters) error {
+// Each run returns a single RunResult, which contains the best value obtained
+// and the corresponding Chromosome.
+type RunResult struct {
+	FitVal     float64
+	Chromosome Chromosome
+}
+
+type byFitness []*RunResult
+
+func (r byFitness) Len() int           { return len(r) }
+func (r byFitness) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r byFitness) Less(i, j int) bool { return r[i].score < r[j].score }
+
+// Sorts results by best fitness value
+func SortResults(rs []*RunResult) {
+	sort.Sort(sort.Reverse(byFitness(rs)))
+}
+
+// This is the main function for running the algorithm.
+func Run(p *Parameters) (*RunResult, error) {
 	for gen := 0; gen < int(p.MaxGens); gen++ {
 		// Generate initial population
 		cf := randChromosomeFunc(int(p.ChromChars), int(p.ChromLen))
@@ -63,5 +97,5 @@ func Run(p *Parameters) error {
 		// TODO(ben): Breed
 		_ = breeders
 	}
-	return nil
+	return nil, nil
 }
