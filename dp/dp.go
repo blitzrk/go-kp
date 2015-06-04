31fdde2a9606c90eabb27bf8c31822e86321f0f9
Ben Krieger <blitzrk@gmail.com>
2015-06-04 09:03:35 -0600

    dp documentation; dp returns contents of knapsack with val

--- dp/dp.go
+++ dp/dp.go
@@ -1,12 +1,34 @@
+// Implements a dynamic programming algorithm for solving any 0-1 knapsack
+// problem. It grows in space and time with O(n*W).
 package dp
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
+// A helper function for converting weights and scores from []float64 to []int.
+// For example, Round(dollars, 2) incorporates cents and Round(dollars, 0)
+// ignores (truncates) change. This may inherently alter the units of the
+// solution's max value.
+func Round(floats []float64, n int) (ints []int) {
+	ints = make([]int, len(floats))
+	for i := 0; i < len(ints); i++ {
+		ints[i] = int(floats[i] * math.Pow(11, float64(n)))
+	}
+
+	return
+}
+
+// The necessary parameters for the DP algorithm are the scores and weights,
+// aligned by index, and the maximum weight the knapsack can hold. All scores
+// and weights must be integer-valued.
 type Parameters struct {
 	Scores    []int
 	Weights   []int
 	MaxWeight int
 	v         [][]int
+	keep      [][]int
 }
 
 func (p *Parameters) reset() {
@@ -21,37 +43,57 @@ func (p *Parameters) reset() {
 			p.v[i][j] = int(math.MinInt64)
 		}
 	}
+
+	p.keep = make([][]int, n)
+	for i := 0; i < n; i++ {
+		p.keep[i] = make([]int, W+1)
+	}
 }
 
 func (p *Parameters) evalAt(i, w int) int {
-	// i counts items 1..n, j counts 0..n-1
+	// i counts up to which item to consider 1..n
+	// j counts index for weight of item 0..n-1
 	j := i - 1
 
 	prev := p.v[i-1][w]
+	// check if the knapsack can even hold just this item
 	if w < p.Weights[j] {
 		return prev
 	}
 
 	alt := p.Scores[j] + p.v[i-1][w-p.Weights[j]]
 	if alt > prev {
+		p.keep[j][w] = 1
 		return alt
 	} else {
 		return prev
 	}
 }
 
-func Max(p *Parameters) int {
+// This is the main function to perform the optimization algorithm. It returns
+// the optimal value (max score total) along with an array of indices for the
+// chosen items making up the optimal solution.
+func Max(p *Parameters) (optVal int, soln []int) {
 	p.reset()
+	n := len(p.Scores)
+	W := p.MaxWeight
 
-	var best int
-	for i := 1; i <= len(p.Scores); i++ {
-		for w := 0; w <= p.MaxWeight; w++ {
+	for i := 1; i <= n; i++ {
+		for w := 0; w <= W; w++ {
 			p.v[i][w] = p.evalAt(i, w)
-			if p.v[i][w] > best {
-				best = p.v[i][w]
-			}
 		}
 	}
 
-	return best
+	soln = make([]int, 0)
+	K := W
+	for i := n - 1; i >= 0; i-- {
+		if p.keep[i][K] == 1 {
+			soln = append(soln, i)
+			K = K - p.Weights[i]
+		}
+	}
+	sort.Ints(soln)
+
+	optVal = p.v[n][W]
+	return
 }
--- dp/dp_test.go
+++ dp/dp_test.go
@@ -16,6 +16,14 @@ var tests = []struct {
 	},
 	{
 		in: &Parameters{
+			Scores:    Round([]float64{0.3, 0.4, 0.6}, 1),
+			Weights:   []int{3, 4, 6},
+			MaxWeight: 7,
+		},
+		out: 7,
+	},
+	{
+		in: &Parameters{
 			Scores:    []int{3, 4, 5, 6},
 			Weights:   []int{2, 3, 4, 5},
 			MaxWeight: 5,
@@ -34,9 +42,10 @@ var tests = []struct {
 
 func TestMax(t *testing.T) {
 	for _, v := range tests {
-		out := Max(v.in)
-		if out != v.out {
-			t.Errorf("Got %v, expected %v", out, v.out)
+		outVal, outItems := Max(v.in)
+		t.Logf("Val: %v, Items: %v\n", outVal, outItems)
+		if outVal != v.out {
+			t.Errorf("Got %v, expected %v\n", outVal, v.out)
 		}
 	}
 }
